eaf04408f0831fe42c4a9f3f7d1c48b9a601fdaf
Andrew V. Jones <andrew.jones@vector.com>
2019-02-19 10:02:59 +0000

    Reworking the Windows patch mechanism to not be dependant upon a specific revision. Patches are now applied against any revision, as long as they apply cleanly. Otherwise, compliation does not continue.
    Signed-off-by: Andrew V. Jones <andrew.jones@vector.com>

--- contrib/setup-cadical.sh
+++ contrib/setup-cadical.sh
@@ -7,20 +7,14 @@ CADICAL_DIR=${DEPS_DIR}/cadical
 # Download and build CaDiCaL
 git clone --depth 1 https://github.com/arminbiere/cadical.git ${CADICAL_DIR}
 cd ${CADICAL_DIR}
-case "$(uname -s)" in
-   CYGWIN*|MINGW32*|MSYS*)
-     revision=58331fd
-     echo ""
-     echo " *** WARNING: Building CaDiCaL on Windows relies on a specific"
-     echo " ***          revision (${revision})."
-     echo " ***          This version of Boolector may be built against an"
-     echo " ***          older version of CaDiCaL."
-     echo ""
-     git reset --hard "${revision}"
-     patch -p1 < ../../contrib/windows_patches/cadical_"${revision}".patch
-     EXTRA_FLAGS="-q"
-     ;;
-esac
+
+if is_windows; then
+  component="CaDiCaL"
+  last_patch_date="20190110"
+  test_apply_patch "${component}" "${last_patch_date}"
+  EXTRA_FLAGS="-q"
+fi
+
 CXXFLAGS="-fPIC" ./configure ${EXTRA_FLAGS}
 make -j${NPROC}
 install_lib build/libcadical.a
